e5e922edb3d1fe935384e498da1dd5dd6ce95d4b
Nidhimeena123 <nidhimeenasm123@gmail.com>
2024-10-02 14:44:48 +0530

    react

--- React_Advance_React-Styling-Routing/module2_1/src/App.js
+++ React_Advance_React-Styling-Routing/module2_1/src/App.js
@@ -1,17 +1,27 @@
-import React from "react";
-import Posts from "./Pages/Posts/Posts";
-import { Routes, Route } from "react-router-dom";
-import Post from "./Pages/Post/Post";
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Posts />} />
-        <Route path="/post/:id" element={<Post />} />
-      </Routes>
-    </>
+    <div className="App">
+      <h1>React JS CRUD Opertations</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EmpListing />}></Route>
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+
+          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-};
+
+}
 
 export default App;
